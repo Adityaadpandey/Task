5131a29ead07099d14955289be8dcb5cabc4ea6a
Aditya Dutt Pandey <adityapandeyadp@gmail.com>
2025-08-13 06:54:04 +0530

    cron job compl

---
+++ apps/go-boilerplate/internal/repository/repositories.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"github.com/adityaadpandey/go-boilerplate/internal/server"
+)
+
+type Repositories struct {
+	Todo     *TodoRepository
+	Comment  *CommentRepository
+	Category *CategoryRepository
+}
+
+func NewRepositories(s *server.Server) *Repositories {
+	return &Repositories{
+		Todo:     NewTodoRepository(s),
+		Comment:  NewCommentRepository(s),
+		Category: NewCategoryRepository(s),
+	}
+}
---
+++ apps/go-boilerplate/internal/service/services.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/adityaadpandey/go-boilerplate/internal/lib/aws"
+	"github.com/adityaadpandey/go-boilerplate/internal/lib/job"
+	"github.com/adityaadpandey/go-boilerplate/internal/repository"
+	"github.com/adityaadpandey/go-boilerplate/internal/server"
+)
+
+type Services struct {
+	Auth     *AuthService
+	Job      *job.JobService
+	Todo     *TodoService
+	Comment  *CommentService
+	Category *CategoryService
+}
+
+func NewServices(s *server.Server, repos *repository.Repositories) (*Services, error) {
+	authService := NewAuthService(s)
+	s.Job.SetAuthService(authService)
+	awsClient, err := aws.NewAWS(s)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create AWS client: %w", err)
+	}
+	return &Services{
+		Job:      s.Job,
+		Auth:     authService,
+		Category: NewCategoryService(s, repos.Category),
+		Comment:  NewCommentService(s, repos.Comment, repos.Todo),
+		Todo:     NewTodoService(s, repos.Todo, repos.Category, awsClient),
+	}, nil
+}
